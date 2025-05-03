e292c7e6b021e11cdc1e23f0f99eb1a4a45abcca
Luis Ghidelli <luisfernando.ghidelli@gmail.com>
2025-05-03 12:32:00 +0100

    api working

--- app/services/tmdb_client.rb
+++ app/services/tmdb_client.rb
@@ -3,7 +3,7 @@ require "net/http"
 
 class  TmdbClient
   def self.movie_list
-    url = URI("https://api.themoviedb.org/3/movie/changes?page=1")
+    url = URI("https://api.themoviedb.org/3/movie/popular")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
@@ -13,6 +13,6 @@ class  TmdbClient
     request["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNWJmYjRkOTgyMzQzY2NmOTliNjcwNmE5OGE3MjMxNyIsIm5iZiI6MTc0NTA3NDAxNi4xMjcsInN1YiI6IjY4MDNiNzYwYjEzNDEwY2RjNzk5NGE0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PC0SXjznAN2xLK4v64RC_5xnmA_o8_u9drDKo0GyCsU"
 
     response = http.request(request)
-    puts response.read_body
+    JSON.parse(response.body.force_encoding("UTF-8"))
   end
 end
