fa050e56a2957ca38cec9717f7c2f3aab41133db
Luis Ghidelli <luisfernando.ghidelli@gmail.com>
2025-05-16 08:10:26 +0100

    fix sign in

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-const flatpickr = require("flatpickr");
-
-document.addEventListener("DOMContentLoaded", () => {
-  flatpickr(".datepicker", {
-    minDate: "today",
-    dateFormat: "Y-m-d"
-  });
-});
